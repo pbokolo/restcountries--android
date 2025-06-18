07e526de76fb54294886220b60238d2a25736d02
pbokolo <dev.pbokolo@gmail.com>
2025-06-18 05:59:25 +0200

    feat(view): Minor bug correction

--- app/src/main/java/com/example/restcountries/ui/CountriesVM.kt
+++ app/src/main/java/com/example/restcountries/ui/CountriesVM.kt
@@ -9,7 +9,6 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlin.random.Random
 
 class CountriesVM:  ViewModel(){
 
