b72515ff684f2cbcefe89aa71eafbd71deeee870
pbokolo <dev.pbokolo@gmail.com>
2025-06-19 18:33:57 +0200

    feat(ui): modified the CountriesVM.kt to use the RestCountriesRepository object in order to make network calls

--- app/src/main/java/com/example/restcountries/ui/CountriesVM.kt
+++ app/src/main/java/com/example/restcountries/ui/CountriesVM.kt
@@ -1,16 +1,36 @@
 package com.example.restcountries.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.restcountries.RestCountriesApplication
+import com.example.restcountries.data.RestCountriesRepository
+import com.example.restcountries.model.toCountry
 import com.example.restcountries.utils.DataLoadingStates
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
 
-class CountriesVM:  ViewModel(){
+class CountriesVM(private val restCountriesRepo: RestCountriesRepository):  ViewModel(){
+
+     companion object {
+         val Factory: ViewModelProvider.Factory = viewModelFactory {
+             initializer {
+                 val application = (this[APPLICATION_KEY] as RestCountriesApplication)
+                 val countriesRepo = application.container.restCountriesRepository
+                 CountriesVM(restCountriesRepo =  countriesRepo)
+             }
+         }
+     }
 
     // Private mutable version of the ui state
     private val _uiState = MutableStateFlow(UiState())
@@ -23,24 +43,49 @@ class CountriesVM:  ViewModel(){
 
     fun getCountries(){
         viewModelScope.launch {
-            // Set loading state before the delay
-            _uiState.update { it.copy(dataLoadingState = DataLoadingStates.Loading.name) }
+            try {
+                // Set loading state before the delay
+                _uiState.update { it.copy(dataLoadingState = DataLoadingStates.Loading.name) }
 
-            delay(3000) // Simulate a network call that takes 3 seconds
+                val countriesDtos = restCountriesRepo.getAll()
+                val countries = countriesDtos.map { it.toCountry() }
 
-            _uiState.update {
-                it.copy(dataLoadingState = DataLoadingStates.Ready.name)
+                _uiState.update {
+                    it.copy(dataLoadingState = DataLoadingStates.Ready.name)
+                }
+
+            }catch (e: IOException){
+                _uiState.update {
+                    it.copy(
+                        dataLoadingState = DataLoadingStates.Error.name,
+                        errorMessage = e.message.toString()
+                    )
+                }
+            }catch (e: HttpException){
+                _uiState.update {
+                    it.copy(
+                        dataLoadingState = DataLoadingStates.Error.name,
+                        errorMessage = e.message.toString()
+                    )
+                }
+            }catch (e: TimeoutCancellationException){
+                _uiState.update {
+                    it.copy(
+                        dataLoadingState = DataLoadingStates.Error.name,
+                        errorMessage = e.message.toString()
+                    )
+                }
+            }
+            catch (e: Exception) {
+                _uiState.update {
+                    it.copy(
+                        dataLoadingState = DataLoadingStates.Error.name,
+                        errorMessage = e.message.toString()
+                    )
+                }
             }
 
-            // After delay, randomly updates with Ready or Error(--Test only)
-            /*_uiState.update {
-                it.copy(
-                    dataLoadingState = if (Random.nextBoolean())
-                        DataLoadingStates.Ready.name
-                    else
-                        DataLoadingStates.Error.name
-                )
-            }*/
+
         }
 
     }
