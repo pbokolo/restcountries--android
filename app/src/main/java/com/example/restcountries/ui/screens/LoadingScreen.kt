d47939e35c1be3a737b344f126102933822068a9
pbokolo <dev.pbokolo@gmail.com>
2025-06-21 14:09:33 +0200

    Using a simple CircularSpinner in the LoadingScreen.kt

--- app/src/main/java/com/example/restcountries/ui/screens/LoadingScreen.kt
+++ app/src/main/java/com/example/restcountries/ui/screens/LoadingScreen.kt
@@ -1,18 +1,16 @@
 package com.example.restcountries.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun LoadingScreen(){
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center
-        ) {
-        Text(text = "Loading...")
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+        CircularProgressIndicator()
     }
 }
+
