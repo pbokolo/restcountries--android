07c084203455a13168ba10af089b9f1e9200700b
pbokolo <dev.pbokolo@gmail.com>
2025-06-22 07:16:47 +0200

    Showing the CountryCard.kt in the CountriesListScreen.kt

--- app/src/main/java/com/example/restcountries/ui/components/CountryCard.kt
+++ app/src/main/java/com/example/restcountries/ui/components/CountryCard.kt
@@ -1,5 +1,46 @@
 package com.example.restcountries.ui.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.example.restcountries.model.Country
+
+@Composable
+fun CountryCard(country: Country, onClick: () -> Unit) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        modifier = Modifier.clickable {
+            onClick()
+        }.height(240.dp)
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box(modifier = Modifier.fillMaxWidth().weight(0.4f)){
+            }
+
+            Column {
+                Text(
+                    text = country.name,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+        }
+
+    }
+}
+
 
 
 
--- app/src/main/java/com/example/restcountries/ui/screens/CountriesListScreen.kt
+++ app/src/main/java/com/example/restcountries/ui/screens/CountriesListScreen.kt
@@ -3,18 +3,24 @@ package com.example.restcountries.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.restcountries.model.Country
+import com.example.restcountries.ui.components.CountryCard
 
 @Composable
 fun CountriesListScreen(list: List<Country>, onClick: () -> Unit){
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
-        Text(text = "${list.size} countries retrieved")
-        Button(onClick = onClick) {
-            Text(text = "Next")
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 160.dp)
+    ) {
+        if(!list.isEmpty()){
+            itemsIndexed(list) { i, c -> CountryCard(country = c, onClick = onClick) }
         }
     }
 }
