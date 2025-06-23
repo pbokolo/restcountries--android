3d290d82e283bf635dda43559edcae92b301dde8
pbokolo <dev.pbokolo@gmail.com>
2025-06-23 05:59:22 +0200

    feat(view): minor change in the CountryDetailsScreen.kt

--- app/src/main/java/com/example/restcountries/ui/screens/CountryDetailsScreen.kt
+++ app/src/main/java/com/example/restcountries/ui/screens/CountryDetailsScreen.kt
@@ -1,10 +1,11 @@
 package com.example.restcountries.ui.screens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.restcountries.model.Country
 
@@ -12,9 +13,13 @@ import com.example.restcountries.model.Country
 fun CountryDetailsScreen(country: Country?){
     Column(modifier = Modifier.fillMaxSize()) {
         if(country == null){
+            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {  }
             Text(text = "No country selected")
         }else{
-            Text(text = country.name)
+            Column(modifier = Modifier.fillMaxSize()) {
+                Text(text = country.name)
+            }
+
         }
     }
 }
