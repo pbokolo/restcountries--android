636c08dd7d53fa59be5ac0d0062302c72273959a
pbokolo <dev.pbokolo@gmail.com>
2025-06-23 06:20:11 +0200

    feat(view): Stylling up the ErrorScreen.kt

--- app/src/main/java/com/example/restcountries/ui/screens/ErrorScreen.kt
+++ app/src/main/java/com/example/restcountries/ui/screens/ErrorScreen.kt
@@ -1,20 +1,46 @@
 package com.example.restcountries.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-
-
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.decode.GifDecoder
+import coil.request.ImageRequest
+import com.example.restcountries.R
 
 @Composable
 fun ErrorScreen(message: String){
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center
+    Box(
+        modifier = Modifier.fillMaxSize().padding(start = 16.dp, end = 16.dp),
+        contentAlignment = Alignment.Center,
     ) {
-        Text(text = message)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+
+        ) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(R.drawable.app_error) // Your GIF
+                    .decoderFactory(GifDecoder.Factory())
+                    .build(),
+                contentDescription = "Loading...",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(120.dp)
+            )
+
+            Text(text = message)
+        }
+
     }
 }
