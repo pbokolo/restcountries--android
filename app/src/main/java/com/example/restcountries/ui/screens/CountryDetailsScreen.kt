85ee7d9b34aa61b6285803ddc1185ac5c269ad6f
pbokolo <dev.pbokolo@gmail.com>
2025-06-24 05:55:44 +0200

    feat(view): Updating the style of the CountryDetailsScreen.kt

--- app/src/main/java/com/example/restcountries/ui/screens/CountryDetailsScreen.kt
+++ app/src/main/java/com/example/restcountries/ui/screens/CountryDetailsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -30,9 +31,9 @@ fun CountryDetailsScreen(country: Country?) {
                 url = country.flagUrl,
                 description = country.name,
                 contentScale = ContentScale.FillWidth,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth().weight(.5f)
             )
-            Column(modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp)) {
+            Column(modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp).weight(.6f)) {
                 Text(
                     text = country.name,
                     maxLines = 1,
